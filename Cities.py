30db8cedef926eb64afc3083ea92467d21daebd1
Nadiya Strus <nadiyakh87@gmail.com>
2016-08-23 20:34:43 +0300

    add changes to NewTest for pagraf test

---
+++ Cities.py
@@ -0,0 +1,46 @@
+from random import randint
+
+limit = int(input("Enter boundary:"))
+cities = []
+ways = []
+destination = int(input("enter city's number:"))
+firstCity = int(input("enter city's number:"))
+k = 0
+   
+   
+for i in range(0, limit):
+    cities.append(randint(0, limit))
+    ways.append(randint(0, limit)  
+    dic_ways = zip(cities,ways)
+
+print (list (direct_ways))
+
+if destination not in (cities or ways):
+	print("Not in list")
+elif firstCity not in (cities or ways):
+	print("Not in list")
+else 
+	print ("Something is wrong")
+
+
+
+for i, j in direct_ways:
+	if direct_ways[i] = destination:
+		if direct_ways[j] = firstCity:
+ 	 		print ("Direct way )) 1 way")
+ 	elif
+
+
+ 	while len(t)<x:
+		c+=1
+		for b in range(1, c):
+			for a in range (1,b):
+				if a**2 + b**2 == c**2
+					t.append([a,b,c])
+	return t
+print(triplets(x))
+
+
+
+
+      
---
+++ NewTest.py
@@ -0,0 +1,7 @@
+albhabet = "asdfghjklqwertyuiopzxcvbnm"
+
+
+
+def check(text):
+	return set(text.lower()).issuperset(set(alphabet))
+	print ("test")
